70116fcba0d93a572b5048efba4d281235afcd13
DPontes <dpontes@protonmail.com>
2021-03-30 23:37:24 +0200

    fixed error with the borderLine smart pointer

--- src/snake.cpp
+++ src/snake.cpp
@@ -75,7 +75,7 @@ void Snake::UpdateBody(SDL_Point &current_head_cell, SDL_Point &prev_head_cell, 
   }
 
   // Check if the snake has gone against the border
-  for (auto const &item : borderLine) {
+  for (auto const item : *borderLine) {
     if (current_head_cell.x == item.x && current_head_cell.y == item.y) {
       alive = false;
     }
